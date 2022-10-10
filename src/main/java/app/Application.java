860904df059ca783384ff3e2a0f6786dbc02bad6
svtart <promixmaxads@gmail.com>
2022-10-10 17:50:46 +0300

    Dog added

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -5,6 +5,8 @@ import app.model.AnimalsCage;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Timer;
+
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,12 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
 
     private Long nanoTime = System.nanoTime();
 
